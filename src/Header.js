e62c387fe4bb867cec8d31c16709a671cffaf459
nyanyaj <ananya.jaianand@gmail.com>
2022-11-09 21:46:05 +0530

    nav bar changes

--- src/Header.js
+++ src/Header.js
@@ -7,16 +7,14 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 function Header() {
   return (
     <div className='header'>
-    <img className="header_logo" src={require("./logo.png")} alt=""/>
-    
-    <IconButton>
+     <a href="/"> <img className="header_logo" src={require("./logo.png")} alt=""/></a> 
+    <a href="/">Furniturely</a>
+    <IconButton id="icon">
         <ShoppingCartIcon/>
     </IconButton>
-    <IconButton>
+    <IconButton id="icon">
         <AccountCircleIcon  />
     </IconButton>
-
-    
     </div>
   )
 }
