93a9686a4a8ff36f50dc6bdf8b90e568e44864dd
nyanyaj <ananya.jaianand@gmail.com>
2022-11-11 08:51:07 +0530

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <div className="d-flex flex-column site-container">
       <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>}></Route>
+          <Route path="/login" element={<LoginScreen/>} />
+        </Routes>
+      </main>
+      <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,39 @@ import "./Header.css"
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
 
 function Header() {
   return (
     <div className='header'>
-     <a href="/"> <img className="header_logo" src={require("./logo.png")} alt=""/></a> 
-    <a href="/">Furniturely</a>
-    <IconButton id="icon">
-        <ShoppingCartIcon/>
-    </IconButton>
-    <IconButton id="icon">
-        <AccountCircleIcon  />
-    </IconButton>
+      <Navbar>
+          <Container>
+
+            <LinkContainer to="/">
+              <Navbar.Brand><img className="header_logo" src={require("./logo.jpeg")} alt=""/></Navbar.Brand>
+            </LinkContainer>
+
+            <Nav className="me-auto  w-100  justify-content-end">
+
+              <IconButton id="icon">
+                <ShoppingCartIcon/>
+              </IconButton>
+
+              <Link to="/login" className="nav-link">
+                <IconButton id="icon">
+                  <AccountCircleIcon  />
+                </IconButton>
+              </Link>
+
+            </Nav>
+            
+          </Container>
+      </Navbar> 
+      
     </div>
   )
 }
