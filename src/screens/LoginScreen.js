3eb94491e49f931809c6780e3c02c10c943c9cb7
nyanyaj <ananya.jaianand@gmail.com>
2022-11-11 20:04:35 +0530

    login and sign up frontend almost done
    resizing
    backend
    phone no. thing

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,25 +1,50 @@
 import { useState } from "react"
 import { Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import "./LoginScreen.css"
 
 const Login=()=>{
    const[email,setEmail]=useState('')
    const[password,setPassword]=useState('') 
-   const handleSubmit=async(e)=>{
+   const handleSubmit= async(e)=>{
     e.preventDefault()
     console.log(email,password)  
    }
    return(
-    <form className="signup" onSubmit={handleSubmit}>
-        <Container className="small-container">
-            <h3>Login</h3>
-            <label>Email:</label>
-            <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email}/>
-            <label>Password:</label>
-            <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
-            <button>Login</button>
-        </Container>
         
-    </form>
+        <section className="sign-in">
+            <div class="container">
+                <div className="signin-content">
+
+                    <div className="login-image">
+                        <figure>
+                            <img id="login_img" class="login_img" src={require("./login_img.png")} alt="login_img"/>
+                        </figure> 
+                    </div> 
+
+                    <div className="login-form">
+                        <br/><br/>
+                        <h3 className="form-title">Login into your account</h3><br/>
+                        <form className="register-form" onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <div class="form-outline w-50">
+                                    <input type="email" placeholder="Email" class="form-control" onChange={(e)=>setEmail(e.target.value)} value={email}/><br/>
+                                    <input type="password" placeholder="Password" class="form-control" onChange={(e)=>setPassword(e.target.value)} value={password}/><br/>
+                                    <button class="btn btn-dark">Login</button>
+                                </div> 
+                            </div>
+                        </form>
+                        <br/>
+                        <Link to="/signup"className="signup-image-link">Do not have a Furniturely account? Create one now!</Link> 
+                    </div>
+
+                   
+                    
+                </div>      
+            </div>
+        </section>
+        
+    
    ) 
 }
 
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,53 @@
 import { useState } from "react"
+import { Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import "./LoginScreen.css"
 
 const Signup=()=>{
-   const[email,setEmail]=useState('')
-   const[password,setPassword]=useState('') 
-   const handleSubmit=async(e)=>{
-    e.preventDefault()
-    console.log(email,password)  
-   }
+    const[name,setName]=useState('')
+    const[email,setEmail]=useState('')
+    const[phone,setPhone]=useState('')
+    const[password,setPassword]=useState('') 
+    const handleSubmit= async(e)=>{
+        e.preventDefault()
+        console.log(name,email,phone,password)  
+    }
    return(
-    <form className="signup" onSubmit={handleSubmit}>
-        <h3>Sign up</h3>
-        <label>Email:</label>
-        <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email}/>
-        <label>Password:</label>
-        <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
-        <button>Signup</button>
-    </form>
+        
+        <section className="sign-up">
+            <div class="container">
+                <div className="signup-content">
+
+                    <div className="login-image">
+                        <figure>
+                            <img id="login_img" class="signup_img" src={require("./signup_img.png")} alt="signup_img"/>
+                        </figure> 
+                    </div> 
+
+                    <div className="signup-form">
+                        <h3 className="form-title">Sign Up Now!</h3><br/>
+                        <form className="register-form" onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <div class="form-outline w-50">
+                                    <input type="text" placeholder="Name" class="form-control" onChange={(e)=>setName(e.target.value)} value={name}/><br/>
+                                    <input type="email" placeholder="Email" class="form-control" onChange={(e)=>setEmail(e.target.value)} value={email}/><br/>
+                                    <input type="number" placeholder="Phone No." class="form-control" onChange={(e)=>setPhone(e.target.value)} value={phone}/><br/>
+                                    <input type="password" placeholder="Password" class="form-control" onChange={(e)=>setPassword(e.target.value)} value={password}/><br/>
+                                    <button class="btn btn-dark">Register</button>
+                                </div> 
+                            </div>
+                        </form>
+                        <br/>
+                        <Link to="/login"className="signup-image-link">Already have a Furniturely account? Login here!</Link> 
+                    </div>
+
+                   
+                    
+                </div>      
+            </div>
+        </section>
+        
+    
    ) 
 }
 
