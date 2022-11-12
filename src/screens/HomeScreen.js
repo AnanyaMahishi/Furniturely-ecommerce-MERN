6413badb33ab68c6beeaedd81aa7cc1c1534c6a8
nyanyaj <ananya.jaianand@gmail.com>
2022-11-12 10:06:09 +0530

    carousel

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,49 @@
-function HomeScreen(){
-    return <div>Home Screen</div>
+import React, {useState} from "react"; 
+import Carousel from 'react-bootstrap/Carousel';
+
+const data = [
+  {
+   image: require('./c1.jpg'), 
+   caption:"Caption",
+   description:"Description Here"
+  },
+  {
+    image:require('./c2.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
+   {
+    image:require('./c3.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   } 
+]
+
+function HomeScreen() {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+       {data.map((slide, i) => {
+        return (
+          <Carousel.Item>        
+        <img
+          className="d-block w-100"
+          src={slide.image}
+          alt="slider image"
+        />
+        <Carousel.Caption>
+          <h3>{slide.caption}</h3>
+          <p>{slide.description}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+        )
+      })}
+      
+    </Carousel>
+  );
 }
 export default HomeScreen;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import { Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import "./LoginScreen.css"
 
